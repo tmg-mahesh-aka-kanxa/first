afd1f551a76c4cda112d35e211312de1bd649725
Mahesh KANXA <maheshtamang380@gmail.com>
2026-04-01 13:48:37 +0545

    code updated

--- README.md
+++ README.md
@@ -3,3 +3,20 @@ this is first repository
 <br>
 Author - Mahesh Tamang AKA KANXA
 
+
+
+    WIN
+user             bot
+rock-----------scissor
+
+paper----------rock
+
+scissor--------paper
+
+    LOSE
+user              bot
+rock-------------paper
+
+paper------------scissor
+
+scissor----------rock
--- one.py
+++ one.py
@@ -3,27 +3,37 @@ scissor = 1
 paper = 2
 rock = 3
 '''
-one = "scissor"
-two = "paper"
-three = "rock"
+scissor = "scissor"
+paper = "paper"
+rock = "rock"
+playagain = "sadf"
 
 import random
+def mygame():
+    print("WELCOME TO MY SCISSOR PAPER ROCK GAME !!!!!!!!")
 
-print("WELCOME TO MY SCISSOR PAPER ROCK GAME !!!!!!!!")
+    userinput = input("Enter your choice :-- ")
 
-userinput = input("Enter your choice :-- ")
+    bot = random.choice(["rock","paper","scissor"])
+    print("Bot chose :-- ",bot)
 
-bot = random.choice(["rock","paper","scissor"])
-print("Bot chose :-- ",bot)
+    if(userinput == bot) :
+        print("TIE !!!!!!!")
 
-if(userinput == bot) :
-    print("TIE !!!!!!!")
+    elif(bot == scissor and userinput == rock or bot == rock and userinput == paper or bot == paper and userinput == scissor):
+        print("(°_°) YOU WIN !!!!!!!!")
 
-elif(bot == one and userinput == two or bot == two and userinput == one or bot == three and userinput == one):
-    print("YOU LOSE !!!!!!!!")
+    elif(userinput == rock and bot == paper or userinput == paper and bot == scissor or userinput == scissor and bot == rock):
+        print("(°n°) YOU LOSE !!!!!!!!!")
 
-elif(userinput == 1 and bot == two or userinput == two and bot == one or userinput == three and bot == one):
-    print("YOU WIN !!!!!!!!!")
+    else :
+        print("invalid input")
 
-else :
-    print("invalid input")
+    print("Press enter to play again")
+    playagain = input()
+
+
+
+
+while playagain == "":
+    mygame()
